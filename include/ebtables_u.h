9fff3d5f9da00255463d28b38d688c25025b7fb1
Baruch Siach <baruch@tkos.co.il>
2018-05-04 12:46:52 +0300

    include: Fix musl libc compatibility
    Conflicting definitions of struct ethhdr between the kernel and musl
    libc provides headers causes a build failure:
    
    In file included from .../usr/include/netinet/ether.h:8:0,
                     from useful_functions.c:28:
    .../usr/include/netinet/if_ether.h:107:8: error: redefinition of ‘struct ethhdr’
     struct ethhdr {
            ^~~~~~
    In file included from include/linux/netfilter_bridge.h:8:0,
                     from include/linux/netfilter_bridge/ebtables.h:17,
                     from include/ebtables_u.h:27,
                     from useful_functions.c:25:
    include/linux/if_ether.h:119:8: note: originally defined here
     struct ethhdr {
            ^~~~~~
    
    Recent enough versions kernel headers allow the libc to suppress
    conflicting kernel definitions. Include the libc proivded
    netinet/ether.h before kernel headers to suppress the conflicting
    definition of struct ethhdr.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/ebtables_u.h
+++ include/ebtables_u.h
@@ -24,6 +24,7 @@
 #ifndef EBTABLES_U_H
 #define EBTABLES_U_H
 #include <netinet/in.h>
+#include <netinet/ether.h>
 #include <linux/netfilter_bridge/ebtables.h>
 #include <linux/netfilter/x_tables.h>
 
